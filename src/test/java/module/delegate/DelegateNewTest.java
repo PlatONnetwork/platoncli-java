f0f9cb0b2716c88aa0fb6b8f318e2f04619505d4
haypo <haypo@live.cn>
2021-03-08 13:04:02 +0800

    新增测试：使用sdk里的委托，该委托会报错

--- src/test/java/module/delegate/DelegateNewTest.java
+++ src/test/java/module/delegate/DelegateNewTest.java
@@ -1,8 +1,18 @@
 package module.delegate;
 
+import com.alaya.contracts.ppos.DelegateContract;
+import com.alaya.contracts.ppos.dto.enums.StakingAmountType;
+import com.alaya.crypto.Credentials;
+import com.alaya.crypto.WalletUtils;
+import com.alaya.protocol.Web3j;
+import com.alaya.protocol.http.HttpService;
 import com.cicdi.jcli.Main;
+import com.cicdi.jcli.util.Common;
+import com.cicdi.jcli.util.ConvertUtil;
 import com.cicdi.jcli.util.QrUtil;
 import org.junit.Test;
+
+import java.util.Scanner;
 
 /**
  * @author haypo
@@ -56,4 +66,12 @@ public class DelegateNewTest {
         };
         Main.main(argv);
     }
+
+    @Test
+    public void testDelegateNewOnline2() throws Exception {
+        Credentials credentials = WalletUtils.loadCredentials(new Scanner(System.in).nextLine(), "E:/codes/IdeaProjects/block_chain_git/trunk/alaya-util-springboot/keystore_core/reward.json");
+        DelegateContract.load(Web3j.build(new HttpService(Common.DEFAULT_NODE_URL)), credentials, 201018).delegate(
+                "0x89ca7ccb7fab8e4c8b1b24c747670757b9ef1b3b7631f64e6ea6b469c5936c501fcdcfa7fef2a77521072162c1fc0f8a1663899d31ebb1bc7d00678634ef746c", StakingAmountType.FREE_AMOUNT_TYPE, ConvertUtil.hrp2Von("0.1")).send().getTransactionReceipt();
+    }
+
 }
