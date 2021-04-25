b51993c4ccc3fc533e092218c51ed127fd219205
haypo <haypo@live.cn>
2021-04-25 16:17:11 +0800

    增加一些测试用例

--- src/test/java/module/hedge/CreateRestrictingPlanTest.java
+++ src/test/java/module/hedge/CreateRestrictingPlanTest.java
@@ -1,12 +1,19 @@
 package module.hedge;
 
 import com.cicdi.jcli.Main;
+import module.TestCommon;
 import org.junit.Test;
 
 public class CreateRestrictingPlanTest {
     @Test
     public void help() {
         String[] args = ("hedge_createRestrictingPlan -help").split(" ");
+        Main.main(args);
+    }
+
+    @Test
+    public void test() {
+        String[] args = ("hedge_createRestrictingPlan -p param/CreateRestrictingPlan.json -d " + TestCommon.rewardWalletDir).split(" ");
         Main.main(args);
     }
 
