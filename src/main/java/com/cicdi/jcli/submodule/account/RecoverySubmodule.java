ec50b6ca23e0eb1e868efc21318d231f7e025492
haypo <haypo@live.cn>
2021-08-16 15:31:39 +0800

    恢复钱包时不再产生恢复文件

--- src/main/java/com/cicdi/jcli/submodule/account/RecoverySubmodule.java
+++ src/main/java/com/cicdi/jcli/submodule/account/RecoverySubmodule.java
@@ -4,12 +4,8 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.cicdi.jcli.submodule.AbstractSimpleSubmodule;
-import com.cicdi.jcli.util.JsonUtil;
-import com.cicdi.jcli.util.ResourceBundleUtil;
-import com.cicdi.jcli.util.StringUtil;
-import com.cicdi.jcli.util.WalletUtil;
+import com.cicdi.jcli.util.*;
 import com.platon.crypto.WalletFile;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 
@@ -26,6 +22,12 @@ public class RecoverySubmodule extends AbstractSimpleSubmodule {
     @Parameter(names = {"--type", "-type", "-t"}, descriptionKey = "account.recovery.type", required = true)
     protected Type type;
 
+    @Override
+    public String run(JCommander jc, String... argv) throws Exception {
+        type.process();
+        return Common.SUCCESS_STR;
+    }
+
     enum Type {
         /**
          * m, mnemonic: 助记词
@@ -33,31 +35,25 @@ public class RecoverySubmodule extends AbstractSimpleSubmodule {
          */
         m, mnemonic, pk, p, privateKey;
 
-        public String process()
+        public void process()
                 throws Exception {
             switch (this) {
                 case m:
-                case mnemonic:
+                case mnemonic: {
                     String mnemonic = StringUtil.readMnemonic();
                     String password = StringUtil.readPasswordTwice();
                     WalletFile walletFile = WalletUtil.genWalletByMnemonic(mnemonic, password);
-                    String recoveryFilename = walletFile.getAddress() + ".mnemonic.recovery";
-                    recoveryFilename = JsonUtil.writeJsonFileWithNoConflict(RECOVERY_DIR, recoveryFilename, walletFile);
-                    return recoveryFilename;
-                default:
+                    StringUtil.info("%s: %s", ResourceBundleUtil.getTextString("RecoverySubmodule.text1"), JsonUtil.toPrettyJsonString(walletFile));
+                    break;
+                }
+                default: {
                     String pk = StringUtil.readPrivateKey();
-                    password = StringUtil.readPasswordTwice();
-                    walletFile = WalletUtil.genWalletByPrivateKey(pk, password);
-                    recoveryFilename = walletFile.getAddress() + ".privateKey.recovery";
-                    recoveryFilename = JsonUtil.writeJsonFileWithNoConflict(RECOVERY_DIR, recoveryFilename, walletFile);
-                    return recoveryFilename;
+                    String password = StringUtil.readPasswordTwice();
+                    WalletFile walletFile = WalletUtil.genWalletByPrivateKey(pk, password);
+                    StringUtil.info("%s: %s", ResourceBundleUtil.getTextString("RecoverySubmodule.text1"), JsonUtil.toPrettyJsonString(walletFile));
+                    break;
+                }
             }
         }
-    }
-
-    @Override
-    public String run(JCommander jc, String... argv) throws Exception {
-        String recoveryFilename = type.process();
-        return ResourceBundleUtil.getTextString("RecoverySubmodule.text1") + ": " + recoveryFilename;
     }
 }
